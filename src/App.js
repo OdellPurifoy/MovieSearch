1700fb2f6f2d3bffc1a036e5f842150d39051678
Odell <odell.purifoy@gmail.com>
2018-09-12 21:21:13 -0400

    Finished building view within the app using jsx

--- src/App.js
+++ src/App.js
@@ -7,20 +7,27 @@ class App extends Component {
     console.log("This is my initializer")
 
     const movies = [
-      {id: 0, title: "Avengers: Infinity War", overview: "Iron Man, Thor, the Hulk and the rest of the Avengers unite to battle their most powerful enemy yet -- the evil Thanos."},
-      {id: 1, title: "The Avengers", overview: "This is my second overview"}
+      {id: 0, poster_src: "avenge_infinity.jpg", title: "Avengers: Infinity War", overview: "Iron Man, Thor, the Hulk and the rest of the Avengers unite to battle their most powerful enemy yet -- the evil Thanos."},
+      {id: 1, poster_src: "avenge.jpg", title: "The Avengers", overview: "This is my second overview"}
     ]
-
-    this.state = {rows: [
-      <p key='0'>This is a row</p>,
-      <p key='1'>This is a row</p>,
-      <p key='2'>This is a row</p>
-    ]}
 
     var movieRows = []
     movies.forEach((movie) => {
       console.log(movie.title)
-      movieRows.push(<p key={movie.id}>{movie.title}</p>)
+      const movieRow = <table key={movie.id} className="movieTable">
+        <tbody>
+          <tr>
+            <td>
+              <img width="100" alt="movie-poster" src={movie.poster_src}/>
+            </td>
+            <td>
+              {movie.title}
+              <p>{movie.overview}</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      movieRows.push(movieRow)
     })
 
     this.state = {rows: movieRows}
